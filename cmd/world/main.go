ccc7c6eed687900639d5e596b0539ed5fb0c4584
Michael Crilly <mcrilly@iix.net>
2016-03-29 14:33:14 +1000

    potato

--- cmd/world/main.go
+++ cmd/world/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Hello, world")
 	})
 
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":"+port, nil)
 }
